39dd5638e5dbebef45f7087546ab19c9d7cd2eab
Largehawiian <jeremoyorama@gmail.com>
2022-07-25 07:50:30 -0400

    test auto deploy

--- main.tf
+++ main.tf
@@ -2,12 +2,29 @@ terraform {
     required_version = ">=0.12.13"
 }
 
-resource "aws_s3_bucket" "terraformS3Bucket"{
-    bucket = "terraforms3bucketjjackson"
-    acl = "private"
-
+resource "aws_instance" "remote_ec2instance" {
+    ami = "ami-092b43193629811af"
+    vpc_security_group_ids = ["sg-0901b34801c8d83a2"]
+    instance_type = "t2.micro"
+    key_name = "ubuntu"
     tags = {
-        Name = "terraforms3bucketjjackson"
-        Environment = "OPS"
+        Name = "remote_ec2instance"
     }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo dnf install -y httpd",
+            "sudo systemctl start httpd",
+            "sudo dnf install -y git",
+            "sudo git clone https://github.com/Largehawiian/DummyWebsite/ /var/www/html"
+        ]
+    
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file("ubuntu.pem")
+    }
+}
 }
